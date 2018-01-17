f0ea4efa879eb617c0de2ea0ba0436889d3a0f6c
Brad Chang <cutebrady@gmail.com>
2018-01-17 10:18:37 +0800

    Performance measurement of demo denoise

--- examples/rnnoise_demo.c
+++ examples/rnnoise_demo.c
@@ -25,35 +25,60 @@
 */
 
 #include <stdio.h>
+#include <time.h>
 #include "rnnoise.h"
 
 #define FRAME_SIZE 480
 
 int main(int argc, char **argv) {
+  clock_t begin = clock();
+
   int i;
   int first = 1;
   float x[FRAME_SIZE];
   FILE *f1, *fout;
-  DenoiseState *st;
-  st = rnnoise_create();
+  DenoiseState *st = rnnoise_create();
+
   if (argc!=3) {
     fprintf(stderr, "usage: %s <noisy speech> <output denoised>\n", argv[0]);
     return 1;
   }
+
   f1 = fopen(argv[1], "r");
   fout = fopen(argv[2], "w");
+  int frame_count = 0;
+
   while (1) {
     short tmp[FRAME_SIZE];
     fread(tmp, sizeof(short), FRAME_SIZE, f1);
-    if (feof(f1)) break;
-    for (i=0;i<FRAME_SIZE;i++) x[i] = tmp[i];
+
+    if (feof(f1))
+      break;
+
+    for (i=0;i<FRAME_SIZE;i++)
+      x[i] = tmp[i];
+
+    // float rnnoise_process_frame(DenoiseState *st, float *out, const float *in)
     rnnoise_process_frame(st, x, x);
-    for (i=0;i<FRAME_SIZE;i++) tmp[i] = x[i];
-    if (!first) fwrite(tmp, sizeof(short), FRAME_SIZE, fout);
+    //float vad_prob = rnnoise_process_frame(st, x, x);
+    //fprintf(stderr, "frame_count:%d, vad_prob:%.3f\n", frame_count++, vad_prob);
+
+    for (i=0;i<FRAME_SIZE;i++)
+      tmp[i] = x[i];
+
+    if (!first)
+      fwrite(tmp, sizeof(short), FRAME_SIZE, fout);
+
     first = 0;
   }
+
   rnnoise_destroy(st);
   fclose(f1);
   fclose(fout);
+
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  fprintf(stderr, "Time Spent: %.2f ms\n", (1000.0 * time_spent));
+
   return 0;
 }
